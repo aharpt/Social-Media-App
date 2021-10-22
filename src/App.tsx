d54ecac4385e55c601d2af7b997f73361248bac9
Aaron Harpt <a169h161@ku.edu>
2021-10-22 18:17:21 -0500

    Add banner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/common/NavBar';
+import Banner from './components/home/Banner';
 import './App.css';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Banner />
     </>
   );
 }
---
+++ src/components/home/Banner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './banner.css';
+
+function Banner() {
+    return (
+        <section>
+            <h1>Welcome to Our Social Media Site.</h1>
+            <h3>Please Login Using the Form Below:</h3>
+        </section>
+    )
+}
+
+export default Banner
