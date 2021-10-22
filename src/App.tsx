ba50383db5a809dcbad99c18c05be1308242182b
Aaron Harpt <a169h161@ku.edu>
2021-10-22 18:31:04 -0500

    Add preliminary Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/common/NavBar';
 import Banner from './components/home/Banner';
+import LoginForm from './components/home/LoginForm';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <NavBar />
       <Banner />
+      <LoginForm />
     </>
   );
 }
---
+++ src/components/home/LoginForm.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function LoginForm() {
+    const [emailValue, setEmailValue] = React.useState<string>("");
+    const [passwordValue, setPasswordValue] = React.useState<string>("");
+
+    const handleEmailChange = (event : React.ChangeEvent<HTMLInputElement>) : void => {
+        setEmailValue(event.target.value);
+    } 
+
+    const handlePasswordChange = (event : React.ChangeEvent<HTMLInputElement>) : void => {
+        setPasswordValue(event.target.value);
+    } 
+
+    return (
+        <section>
+            <form>
+            <div>
+                <label htmlFor="email">Email: </label>
+                <input type="email" id="email" value={emailValue} onChange={handleEmailChange} />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input type="password" id="password" value={passwordValue} onChange={handlePasswordChange} />
+            </div>
+
+            <button type="button">Submit</button>
+        </form>
+
+        <p>If you do not yet have an account, sign up <a href="/here">here.</a></p>
+        </section>
+    )
+}
+
+export default LoginForm
